daabfc06f898bd0dbb918f2a7c1c8e5579982ef3
nphcuong <nphcuong205@gmail.com>
2025-09-18 21:03:53 +0700

    chrome path cua mac

--- config.py
+++ config.py
@@ -10,14 +10,13 @@ CHECKSUMS_PATH = os.path.join(DOWNLOAD_DIR, "checksums.txt")
 
 PROGRESS_PATH = os.path.join(DOWNLOAD_DIR, "progress.txt")
 
-# Đường dẫn Chrome động cho từng hệ điều hành
 import sys
 CHROME_PATH = os.environ.get("CHROME_PATH")
 if not CHROME_PATH:
 	if sys.platform.startswith("win"):
-		CHROME_PATH = r"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-		# Nếu dùng user cài đặt riêng, có thể cần chỉnh lại
+		# Sửa lại trường PC_Name
+		CHROME_PATH = r"C:\\Users\\PC_Name\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"
 	elif sys.platform == "darwin":
 		CHROME_PATH = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 	else:
-		CHROME_PATH = None  # Để Selenium tự nhận nếu không phải Windows/macOS
+		CHROME_PATH = None
